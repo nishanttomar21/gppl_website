a276463a2b687848eeaadb939d610fc707c04afe
nishanttomar21 <nishanttomar21@gmail.com>
2025-11-30 19:11:26 +0530

    feat: update hero tagline to glowing capsule design

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ command }) => ({
     base: command === 'build' ? '/gppl_website/' : '/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html'),
+                contact: resolve(__dirname, 'contact.html'),
+                newsletter: resolve(__dirname, 'newsletter.html'),
+                product: resolve(__dirname, 'product.html'),
+            },
+        },
+    },
     server: {
         allowedHosts: true
     }
